2937e37a4c37c4b82d08754e0623af96611a6ad9
Kristina Yantser <kristy.net.ua@mail.ru>
2020-10-17 14:14:16 +0300

    1

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	// для ленивой заргурки изображений. для pagespeed
+	[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+		img.setAttribute('src', img.getAttribute('data-src'));
+		img.onload = function() {
+			img.removeAttribute('data-src');
+		};
+	});
+	//
+
+	// Custom JS
+
+});
